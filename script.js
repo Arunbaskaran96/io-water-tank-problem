1d37567b703a6d3a2c24ce913b6f856a9d43b185
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2024-04-20 22:25:02 +0530

    change html and script file

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-//Generate Input Function
-function generateInput() {
-  let input = document.getElementById("input");
-  input.value = "0400060640";
-}
-
-//Submit Handler
-function submitHandler() {
-  let input = document.getElementById("input");
-  if (!input.value) {
-    alert("Enter valid input");
-  } else {
-    let arr = input.value.split("");
-    waterAndBricks(arr);
-    onlyWater(arr);
-    maxWaterHold(arr);
-  }
-}
-
-//function for find maximum amount of water can hold
-function maxWaterHold(arr) {
-  let total = 0;
-  let wallStart;
-  let wallEnd;
-  let count = 0;
+function makeGraph(arr, id) {
+  let conatiner = document.getElementById(id);
+  let graphContainer = document.createElement("div");
+  graphContainer.setAttribute("id", "graph");
+  let yaxis = document.createElement("div");
+  yaxis.setAttribute("class", "yaxis");
+  yaxis.style.width = `${arr.length * 50}px `;
+  graphContainer.setAttribute("class", "graphContaimer");
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = parseInt(arr[i]);
-    if (wallStart && arr[i] === 0) {
-      count++;
-    }
-    if (wallStart && arr[i] > 0) {
-      wallEnd = arr[i];
-      total += Math.min(wallStart, wallEnd) * count;
-      count = 0;
-      wallStart = wallEnd;
-      wallEnd = undefined;
-    }
-    if (parseInt(arr[i]) > 0 && !wallStart) {
-      wallStart = arr[i];
-    }
+    let outerChild = document.createElement("div");
+    outerChild.style.height = `${arr[i].value * 50}px`;
+    outerChild.style.backgroundColor = `${arr[i].bg}`;
+    outerChild.setAttribute("class", "block");
+    graphContainer.appendChild(outerChild);
   }
-  let totalSum = document.getElementById("totalSum");
-  totalSum.innerText = `This input can contain ${total}`;
+  conatiner.appendChild(graphContainer);
+  conatiner.appendChild(yaxis);
 }
 
-//Function for BricksWithWater
+function submitInput() {
+  //checking whether it has child node or not if is there remove it and append new one
+  const container = document.getElementById("brickandwater");
+  if (container.hasChildNodes()) {
+    const graphContainer = document.getElementById("graph");
+    container.removeChild(graphContainer);
+    const yaxis = document.getElementsByClassName("yaxis");
+    container.removeChild(yaxis[0]);
+  }
+
+  const waterContainer = document.getElementById("waterContainer");
+
+  if (waterContainer.hasChildNodes()) {
+    const graphContainer = document.getElementById("graph");
+    waterContainer.removeChild(graphContainer);
+    const yaxis = document.getElementsByClassName("yaxis");
+    waterContainer.removeChild(yaxis[0]);
+  }
+
+  //getting number from input element and convert in to number
+  const inputVal = document.getElementById("input");
+  const arr = inputVal.value.split("").map(Number);
+
+  waterAndBricks(arr);
+  maxWaterHold(arr);
+  onlyWater(arr);
+}
+
 function waterAndBricks(arr) {
   let forwardCase = [];
   let reverseCase = [];
@@ -93,12 +94,35 @@ function waterAndBricks(arr) {
       });
     }
   }
+  makeGraph(result, "brickandwater");
+  const brickElem = document.getElementById("bricks");
+  brickElem.innerText = "Bricks with Water";
+}
 
-  let maximum = Math.max(...arr);
-  for (let i = 0; i < result.length; i++) {
-    createRow(i, maximum, result, "brickwithwater");
+function maxWaterHold(arr) {
+  let total = 0;
+  let wallStart;
+  let wallEnd;
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = parseInt(arr[i]);
+    if (wallStart && arr[i] === 0) {
+      count++;
+    }
+    if (wallStart && arr[i] > 0) {
+      wallEnd = arr[i];
+      total += Math.min(wallStart, wallEnd) * count;
+      count = 0;
+      wallStart = wallEnd;
+      wallEnd = undefined;
+    }
+    if (parseInt(arr[i]) > 0 && !wallStart) {
+      wallStart = arr[i];
+    }
   }
-  document.getElementById("heading1").innerText = "Bricks with Water";
+
+  const h2Elem = document.getElementById("total");
+  h2Elem.innerText = `This input can contain ${total}`;
 }
 
 //Function for Onlywater
@@ -152,33 +176,6 @@ function onlyWater(arr) {
     }
   }
 
-  let maximum = Math.max(...arr);
-  for (let i = 0; i < result.length; i++) {
-    createRow(i, maximum, result, "onlywater");
-  }
-  document.getElementById("heading2").innerText = "Only Water";
-}
-
-//Creating DOM
-function createRow(index, maximum, result, id) {
-  let container = document.getElementById(id);
-  let rowContainer = document.createElement("div");
-  rowContainer.setAttribute("class", "rowContainer");
-  let emptySpace = maximum - result[index].value;
-  for (let i = 0; i < emptySpace; i++) {
-    let child = document.createElement("div");
-    child.setAttribute("class", "child");
-    child.style.height = "50px";
-    child.style.width = "50px";
-    rowContainer.appendChild(child);
-  }
-  for (let i = emptySpace; i < maximum; i++) {
-    let child = document.createElement("div");
-    child.setAttribute("class", "child");
-    child.style.height = "50px";
-    child.style.width = "50px";
-    child.style.backgroundColor = result[index].bg;
-    rowContainer.appendChild(child);
-  }
-  container.appendChild(rowContainer);
+  makeGraph(result, "waterContainer");
+  document.getElementById("water").innerText = "Only Water";
 }
